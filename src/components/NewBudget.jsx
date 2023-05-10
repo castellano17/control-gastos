df9116f288b3ceecadffac4cb16a4b5ad33a32f8
castellano17 <castellano17@gmail.com>
2023-05-09 22:08:12 -0600

    feat(components) add: Message for error validate bugdet initial

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import Message from "./Message";
 
-const NewBudget = () => {
+const NewBudget = ({ budget, setBudget }) => {
+  const [message, setMessage] = useState("");
+
+  const handleBudget = (e) => {
+    e.preventDefault();
+
+    if (!Number(budget) || Number(budget) < 0) {
+      setMessage("no es un presupuesto válido");
+    } else {
+      console.log("es un presupuesto valid");
+    }
+  };
+
   return (
     <div className="container-budget container shadow ">
-      <form className="form">
+      <form onSubmit={handleBudget} className="form">
         <div className="field">
           <label>Definir Presupuesto</label>
           <input
             className="new-budget"
             type="text"
             placeholder="Añade tu presupuesto"
+            value={budget}
+            onChange={(e) => setBudget(e.target.value)}
           />
         </div>
         <input type="submit" value="Añadir" />
+
+        {message && <Message type="error">{message}</Message>}
       </form>
     </div>
   );
