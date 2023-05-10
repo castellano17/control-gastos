7db6bd6544b1f17c74acff8f9fa105db27efb554
castellano17 <castellano17@gmail.com>
2023-05-09 21:32:39 -0600

    feat(components) add: Header, index.css, NewBudget: form submit budget

---
+++ src/components/NewBudget.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NewBudget = () => {
+  return (
+    <div className="container-budget container shadow ">
+      <form className="form">
+        <div className="field">
+          <label>Definir Presupuesto</label>
+          <input
+            className="new-budget"
+            type="text"
+            placeholder="Añade tu presupuesto"
+          />
+        </div>
+        <input type="submit" value="Añadir" />
+      </form>
+    </div>
+  );
+};
+
+export default NewBudget;
